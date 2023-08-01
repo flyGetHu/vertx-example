39e492a151f19f50e40a2fc13c1483638e3a649a
huan <970780868@qq.COM>
2023-08-01 13:50:27 +0800

    重命名模块名称

--- common-core/src/main/kotlin/com/vertx/common/utils/DateUtil.kt
+++ common-core/src/main/kotlin/com/vertx/common/utils/DateUtil.kt
@@ -4,22 +4,16 @@ import cn.hutool.core.date.DateTime
 import cn.hutool.core.date.DateUtil
 
 /**
- * 日期时间工具类
+ * 定义序列化字符串时间格式
+ * 需要考虑兼容多种格式
  */
-class DateUtil : DateUtil() {
-
-    /**
-     * 定义序列化字符串时间格式
-     * 需要考虑兼容多种格式
-     */
-    fun parseDate(dateTime: String): DateTime {
-        return DateUtil.parse(
-            dateTime.split(".")[0],
-            "yyyy-MM-dd HH:mm:ss",
-            "yyyy-MM-dd HH:mm",
-            "yyyy-MM-dd'T'HH:mm:ss",
-            "yyyy-MM-dd'T'HH:mm",
-            "yyyy-MM-dd",
-        )
-    }
+fun parseDate(dateTime: String): DateTime {
+    return DateUtil.parse(
+        dateTime.split(".")[0],
+        "yyyy-MM-dd HH:mm:ss",
+        "yyyy-MM-dd HH:mm",
+        "yyyy-MM-dd'T'HH:mm:ss",
+        "yyyy-MM-dd'T'HH:mm",
+        "yyyy-MM-dd",
+    )
 }
