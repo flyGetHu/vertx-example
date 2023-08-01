06f4f21f604954ba272d5b2177536805aca19387
huan <970780868@qq.COM>
2023-08-01 16:14:10 +0800

    task测试

--- example-task/src/main/kotlin/com/vertx/task/verticle/MainVerticle.kt
+++ example-task/src/main/kotlin/com/vertx/task/verticle/MainVerticle.kt
@@ -3,6 +3,7 @@ package com.vertx.task.verticle
 import cn.hutool.log.StaticLog
 import com.vertx.common.config.VertxLoadConfig
 import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
 import java.time.Instant
 
 class MainVerticle : CoroutineVerticle() {
@@ -11,7 +12,7 @@ class MainVerticle : CoroutineVerticle() {
         try {
             val timer = Instant.now()
             VertxLoadConfig.init()
-            vertx.deployVerticle(TaskVerticle::class.java.name)
+            vertx.deployVerticle(TaskVerticle::class.java.name).await()
             StaticLog.info("启动示例TASK项目成功:${Instant.now().toEpochMilli() - timer.toEpochMilli()}ms")
         } catch (e: Exception) {
             e.printStackTrace()
