86a0a95fd29eb7d341dc85ab57810dd5c47d4b97
huan <970780868@qq.com>
2023-09-15 14:46:32 +0800

    Simplify release.bat and update .gitignore
    In release.bat, we remove the initial command lines "@echo off","chcp 65001", and "setlocal enabledelayedexpansion", and the terminal command "endlocal". This simplifies the build and deployment script, making it easily digestible and maintainable. Updates to the .gitignore file include removal of '*.iws', '*.iml', and '*.ipr' to clean the ignored patterns list, as these files are no longer relevant in our project structure.

--- release.bat
+++ release.bat
@@ -1,8 +1,3 @@
-@echo off
-chcp 65001
-setlocal enabledelayedexpansion
-
 mvn clean install -U package -DskipTests
 
 mvn clean install -U org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
-endlocal
