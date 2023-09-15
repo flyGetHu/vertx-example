682b1b4eb11ff127e6ddd1316906017068356c7e
huan <970780868@qq.com>
2023-09-15 10:37:48 +0800

    由于包重命名而更新项目文件 需要进行几次更新才能与最近对程序包的重命名相对应。这包括更改workspace.xml和compiler.xml中的路径以反映新结构，修改release.bat以调整构建过程，以及在misc.xml中添加编码声明和包引用以保持项目连续性。这些更改的目的是确保所有引用都符合新的命名约定。还添加了一个代表重命名操作的任务以进行跟踪。

--- release.bat
+++ release.bat
@@ -2,30 +2,7 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-rem 项目打包命令
-set PACKAGE_PROJECT= mvn clean install -U package -DskipTests
+mvn clean install -U package -DskipTests
 
-rem 打包项目
-call %PACKAGE_PROJECT%
-
-rem 设置Maven命令
-set MAVEN_COMMAND=mvn clean install -U org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
-
-rem 获取当前目录路径
-set "CURRENT_DIR=%CD%"
-
-rem 遍历当前目录下的子目录
-for /d %%i in (*vertx-common*) do (
-    set "SUB_DIR=%%i"
-    if exist "!SUB_DIR!" (
-        pushd "!SUB_DIR!"
-        echo 进入目录: !CD!
-        call %MAVEN_COMMAND%
-        popd
-        echo 切换回目录: !CURRENT_DIR!
-    ) else (
-        echo 目录不存在: %%i
-    )
-)
-
+mvn clean install -U org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
 endlocal
