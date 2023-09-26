37d86d8c0f7726c521a201dff4808330bbe0654f
huan <970780868@qq.com>
2023-09-26 13:50:38 +0800

    更新 vertx-common-core-kt 版本并更正 pom 文件
    在向 MysqlHelper 类函数添加可选 SqlConnection 参数后，将 vertx-common-core-kt 的版本更新为 1.0.2-SNAPSHOT。此外，还纠正了其他 Vert.x 模块的 pom 文件中“groupId”与“version”的错误替换。此调整在使用 SqlConnection 时提供了灵活性，并在可以重用同一连接时增强了资源和性能优化的潜力。此提交消息与确保这些更改反映在版本控制系统中相关。

--- vertx-common-core-kt/src/main/kotlin/com/vertx/common/entity/mysql/PageResult.kt
+++ vertx-common-core-kt/src/main/kotlin/com/vertx/common/entity/mysql/PageResult.kt
@@ -6,10 +6,14 @@ package com.vertx.common.entity.mysql
  * @param T 页面响应中包含的数据类型
  * @property pageNumber 当前页码
  * @property pageSize 每页的项目数
- * @property totalItems 所有页面的项目总数
+ * @property total 所有页面的项目总数
  * @property totalPages 总页数
  * @property data 本页数据项列表
  */
 data class PageResult<T>(
-    val pageNumber: Int, val pageSize: Int, val totalItems: Long, val totalPages: Int, val data: List<T>
+    val pageNumber: Int,
+    val pageSize: Int,
+    val total: Long,
+    val totalPages: Int,
+    val data: List<T>
 )
