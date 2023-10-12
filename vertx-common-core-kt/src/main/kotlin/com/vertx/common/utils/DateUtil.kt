913b020a0d86d2b6850c6e690ed1ec50d68cc7ea
huan <970780868@qq.com>
2023-10-12 14:33:57 +0800

    增强日期解析以支持多种格式
    DateUtil 中的日期解析方法已得到增强，可以支持多种日期格式。此更改旨在提供与各种日期格式的兼容性。现在，它将尝试使用预定义日期格式列表来解析输入日期，如果没有格式匹配，则返回 null。针对由于非法参数或日期异常导致解析失败的情况添加了异常处理。这确保了该函数可以继续尝试解析其他格式。

--- vertx-common-core-kt/src/main/kotlin/com/vertx/common/utils/DateUtil.kt
+++ vertx-common-core-kt/src/main/kotlin/com/vertx/common/utils/DateUtil.kt
@@ -1,5 +1,6 @@
 package com.vertx.common.utils
 
+import cn.hutool.core.date.DateException
 import cn.hutool.core.date.DateTime
 import cn.hutool.core.date.DateUtil
 
@@ -7,13 +8,29 @@ import cn.hutool.core.date.DateUtil
  * 定义序列化字符串时间格式
  * 需要考虑兼容多种格式
  */
-fun parseDate(dateTime: String): DateTime {
-    return DateUtil.parse(
-        dateTime.split(".")[0],
+fun parseDate(dateTime: String): DateTime? {
+    val formats = listOf(
         "yyyy-MM-dd HH:mm:ss",
         "yyyy-MM-dd HH:mm",
         "yyyy-MM-dd'T'HH:mm:ss",
         "yyyy-MM-dd'T'HH:mm",
         "yyyy-MM-dd",
+        "yyyyMMdd",
+        "yyyyMMddHHmmss",
+        "EEE, dd MMM yyyy HH:mm:ss Z", // RFC 822
+        "EEEE, dd-MMM-yy HH:mm:ss ZZZ", // RFC 850
+        "EEE MMM dd HH:mm:ss yyyy" // ANSI C's asctime()
     )
+
+    for (format in formats) {
+        try {
+            return DateUtil.parse(dateTime, format)
+        } catch (e: IllegalArgumentException) {
+            // ignore and try next format
+        } catch (e: DateException) {
+            // ignore and try next format
+        }
+    }
+
+    return null
 }
