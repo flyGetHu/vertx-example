6e102f18e1ee9b0250a1e70fdc36cb1945606380
huan <970780868@qq.com>
2023-11-01 16:28:25 +0800

    添加 Redis 初始化错误的自定义异常
    引入了自定义异常“RedisInitException”，用于以更精细和更有意义的方式处理 Redis 初始化错误。通过提供更清晰和具体的错误消息，这将有利于日志监控和故障排除。使用此自定义异常替换了“RedisClient.kt”中针对 Redis 相关故障的常见“Exception”调用。

---
+++ vertx-common-redis-kt/src/main/kotlin/com/vertx/redis/RedisInitException.kt
@@ -0,0 +1,10 @@
+package com.vertx.redis
+
+/**
+ * 针对Redis初始化错误的自定义异常类。
+ * 扩展标准异常类。
+ *
+ * @param message 描述异常的错误消息。
+ * @param cause 异常的原因。如果原因未知或不存在，则它可以为 null。
+ */
+class RedisInitException(message: String, cause: Throwable? = null) : Exception(message, cause)
--- vertx-common-redis-kt/src/main/kotlin/com/vertx/redis/client/RedisClient.kt
+++ vertx-common-redis-kt/src/main/kotlin/com/vertx/redis/client/RedisClient.kt
@@ -3,6 +3,7 @@ package com.vertx.redis.client
 import cn.hutool.log.StaticLog
 import com.vertx.common.config.isInit
 import com.vertx.common.config.vertx
+import com.vertx.redis.RedisInitException
 import io.vertx.kotlin.coroutines.await
 import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.redis.client.Redis
@@ -30,12 +31,12 @@ object RedisClient {
      */
     suspend fun init(config: com.vertx.common.entity.app.Redis, isDef: Boolean = true): RedisAPI? {
         if (!isInit) {
-            throw Exception("vertx is not init")
+            throw RedisInitException("vertx is not init")
         }
         val redisOptions = RedisOptions()
         val host = config.host
         if (host.isBlank()) {
-            throw Exception("redis host is blank")
+            throw RedisInitException("redis host is blank")
         }
         val port = config.port
         val database = config.database
