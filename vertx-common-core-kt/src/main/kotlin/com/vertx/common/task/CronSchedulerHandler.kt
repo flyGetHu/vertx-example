7d1ff5774806ca5c219ace2ede5b4c5436250e8e
huan <970780868@qq.com>
2023-11-01 22:25:33 +0800

    重构 CronSchedulerHandler 以包含任务选项
    vertx-common-core-kt 的 CronSchedulerHandler 现在包含急需的任务选项。此更改不仅允许任务在启动时初始化，还包括启动环境，并提供任务执行结果的回调。这使用户可以更轻松地管理任务和处理任何错误，确保应用程序中的任务调度更加稳健和高效。

--- vertx-common-core-kt/src/main/kotlin/com/vertx/common/task/CronSchedulerHandler.kt
+++ vertx-common-core-kt/src/main/kotlin/com/vertx/common/task/CronSchedulerHandler.kt
@@ -39,8 +39,12 @@ interface CronSchedulerHandler {
     /**
      * 启动定时任务
      * Starts the scheduled task.
-     * @param initStart 是否初始化启动 (whether to initialize the task on startup)
-     * @param startEnv 启动环境 (startup environment) 如果不为空,则只有在active为startEnv时才会启动定时任务
+     * @param taskOptions 定时任务配置
+     *  1: 是否初始化启动 (whether to initialize the task on startup)
+     *  2: 启动环境 (startup environment) 如果不为空,则只有在active为startEnv时才会启动定时任务
+     *  3: 定义任务执行完成回调函数
+     *    入参String,如果不为null,则代表定时任务执行失败,会打印错误日志
+     *    此参数可以作为任务执行结果的回调,比如发送邮件通知或者保存到数据库
      */
     fun start(taskOptions: TaskOptions) {
         val startEnv = taskOptions.startEnv
