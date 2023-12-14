9ca526a214311af5b6a167e127d9cc06fbed869c
huan <970780868@qq.com>
2023-12-14 09:46:26 +0800

    重构SharedLockHelper，动态key兼容更多使用场景

--- vertx-common-core-kt/src/main/kotlin/com/vertx/common/entity/mq/MqMessageData.kt
+++ vertx-common-core-kt/src/main/kotlin/com/vertx/common/entity/mq/MqMessageData.kt
@@ -7,10 +7,10 @@ import com.fasterxml.jackson.annotation.JsonProperty
  * 消息数据
  * @param T 消息类型
  * @property id 消息id
- * @property msg 消息
+ * @property body 消息
  */
 class MqMessageData<T>(// 消息
-    @JsonProperty("msg") var msg: T
+    @JsonProperty("body") var body: T
 ) {
     // 消息id
     @JsonProperty("id")
