d7d53aeba8526926608747a9db87bdc8332d64ae
huan <970780868@qq.com>
2024-02-29 15:14:15 +0800

    打包脚本优先打包core包 其他模块会依赖

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 @echo off
+cd vertx-common-core-kt
+echo Executing Maven command in vertx-common-core-kt
+mvn clean install -U -fae -T 1C org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+cd ..
+
 for /D %%i in (vertx-common*) do (
-    cd %%i
-    echo Executing Maven command in %%i
-    mvn clean install -U -fae -T 1C org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
-    cd ..
+    if not "%%i"=="vertx-common-core-kt" (
+        cd %%i
+        echo Executing Maven command in %%i
+        mvn clean install -U -fae -T 1C org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+        cd ..
+    )
 )
